18e23728fa6dad69927a47317fc336dc88cc0fc3
Bright Olayinka <explorebright@gmail.com>
2019-03-02 03:33:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,7 +9,6 @@ function forLoop(array) {
   return array;
 }
 
-
  function whileLoop(n) {
   let countdown = n;
 
@@ -19,39 +18,13 @@ function forLoop(array) {
     return "done";
   }
 
- function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+ function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
   }
-  return array;
+
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
-
- function whileLoop(n) {
-  let countdown = n;
-
-     while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
-
- 
- function doWhileLoop(num) {
-   function incrementVariable()
-   {
-     i = i + 1;
-     return i;
-   }
-    do{
-    console.log("I run once regardless.");
-} while (incrementVariable() < 5);
-
-    
-    
-    
-    
- 
