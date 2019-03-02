cbdfed00ef7b03782fd46880bd973829ee549d3f
Bright Olayinka <explorebright@gmail.com>
2019-03-02 03:14:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,13 +18,4 @@ function forLoop(array) {
     return "done";
   }
 
- function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-   do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
+ 
